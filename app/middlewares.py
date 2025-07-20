6e7ecf5c17e834082b3d269443ec6da099489eb5
M1gueel <miguelguilca1222@gmail.com>
2025-07-20 00:04:58 -0500

    Remove unused encryption key generation and debugging functions from auth.py; clean up middlewares.py by commenting out local frontend URLs

--- app/middlewares.py
+++ app/middlewares.py
@@ -3,9 +3,6 @@ from fastapi.middleware.cors import CORSMiddleware
 
 def add_middlewares(app: FastAPI) -> None:
     origins = [
-        # "http://localhost:5173",
-        # "http://localhost:3000" # URLs del frontend
-        #"*"
         "https://software-seguro-grupo-4-front.vercel.app"
     ]
 #TODO: Cambiar los headers y metodos permitidos
